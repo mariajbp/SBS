912a5676a969f3aa8a7c7a26553e69cdc87f106d
PedroFCM <pedrofcmachado26@gmail.com>
2020-12-30 09:25:51 +0000

    erro no select

--- 2º Projeto/whatmusic/api/SelectMusics.py
+++ 2º Projeto/whatmusic/api/SelectMusics.py
@@ -1,6 +1,4 @@
 """
-Created on Sat Dec 19 17:55:04 2020
-
 @author: GRUPO_1_MLFA
 """
 
@@ -15,8 +13,6 @@ songsClusters = pd.read_csv("Data/Songs_clusters.csv", sep = ",",
                             engine = 'python', encoding = 'utf8')
 genres = pd.read_csv("Data/genres.csv", sep = ",",
                             engine = 'python', encoding = 'utf8')
-
-#print(songsClusters.head())
 
 first = songsNormalized.iloc[0]
 
@@ -61,14 +57,14 @@ def suggestSongs(name, artist):
     songsSugested = songsSugested[(songsSugested['artists'] != first['artists']) & 
                                   (songsSugested['name'] != first['name'])]
     
-    print(songsSugested[['artists', 'name']].head(8))
+    return songsSugested[['artists', 'name']].head(8).to_json(orient = 'split')
 
 
 def suggestSongsByGenre(name):
     
     genre = genres[genres['genres'] == name]
     
-    #print(genre)
+    print(genre)
     
     if genre.empty:
         print('Something went wrong :(')
@@ -77,7 +73,7 @@ def suggestSongsByGenre(name):
     dif = pd.DataFrame()
     
     genre = genre.drop(columns = 'genres')
-    #print(centroids)
+    print(centroids)
     
     for i in range(len(centroids)):
         dif = dif.append(genre, ignore_index = True)
@@ -94,20 +90,97 @@ def suggestSongsByGenre(name):
             min = dif[i]
             cluster = i
     
-    #print("Min: ", min, " and cluster: ", cluster)
+    print("Min: ", min, " and cluster: ", cluster)
     
     
     songsSugested = songsClusters[songsClusters['Cluster'] == ('cluster_' + str(cluster))]
-    ss = songsSugested[['artists', 'name']].head(8).to_json()
-    return(ss)
-    #print(songsSugested[['artists', 'name']].head(8))
+    
+    return songsSugested[['artists', 'name']].head(8)..to_json(orient = 'split')
 
+
+def nearestSongs(name, artist):
+    
+    song = songsNormalized.copy()
+    songWithName = song.copy()
+    song = song[(song['name'] == name) & 
+                (song['artists'] == artist)]
+    
+    if song.empty:
+        print('Song not available... try again')
+        return
+    
+    song = song.iloc[0]
+    
+    dif = pd.DataFrame()
+    
+    song = song.drop(columns = ['artists', 'name'])
+    
+    for i in range(len(centroids)):
+        dif = dif.append(song, ignore_index = True)
+        
+    dif = (pow((centroids - dif), 2))
+    dif = dif.sum(axis = 1)
+    dif = dif.apply(np.sqrt)
+        
+    min = dif[0]
+    cluster = 0
+    
+    for i in range(len(dif)):
+        if dif[i] < min:
+            min = dif[i]
+            cluster = i
+    
+    print('cluster: ', cluster)
+        
+    songsSugested = songsClusters.copy()
+    songsSugested = songsSugested[songsSugested['Cluster'] == ('cluster_' + str(cluster))]
+    songsSugested = songsSugested[(songsSugested['artists'] != first['artists']) & 
+                                  (songsSugested['name'] != first['name'])]
+    
+    songsSugested = songsSugested.drop(columns = 'Cluster')
+    
+    
+    dist = pd.DataFrame()
+    
+    numericValues = ['valence', 'year', 'acousticness', 'danceability',
+       'duration_ms', 'energy', 'instrumentalness', 'liveness', 'loudness',
+       'speechiness', 'tempo', 'Rock', 'Pop', 'Rap/Hip hop',
+       'Eletronic/Dance', 'Latin', 'Indie', 'Classical', 'Country', 'Metal',
+       'Blues', 'Other', 'Soul', 'Jazz', 'Punk']
+    
+    if len(songWithName) > 100:
+        tam = 100
+    else:
+        tam = len(songWithName)
+        
+    for i in range(tam):
+        dist = dist.append(songsSugested[numericValues], ignore_index = True)
+        
+    dist = (pow((songWithName.head(tam)[numericValues] - dist), 2))
+    dist = dist.sum(axis = 1)
+    dist = dist.apply(np.sqrt)
+    
+    dist = dist[dist != 0]
+    dist = dist.sort_values(ascending = True)
+    
+    listSongs = dist.index
+    
+    songs = pd.DataFrame()
+    for i in range(len(listSongs)):
+        songs = songs.append(songsSugested.iloc[listSongs[i]])
+        
+    return songs[['artists', 'name']].head(8).to_json(orient = 'split')
 
 #suggestSongs(first['name'], first['artists'])
 
-#suggestSongs('Give Me Love', 'Ed Sheeran')
-#suggestSongs("Let's Love", 'David Guetta')
-
 #suggestSongs('Toxic', 'Britney Spears')
+#suggestSongs("Black", 'Pearl Jam')
+
+
+#suggestSongsByGenre('Rock')
 
 #suggestSongsByGenre('Pop')
+
+#nearestSongs('Stressed Out', 'Twenty One Pilots')
+#nearestSongs('Ironic - 2015 Remaster', "Alanis Morissette")
+nearestSongs("Black", 'Pearl Jam')
